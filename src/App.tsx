315f1063680d0205157f3152c5e3ac928aac3e0d
Sanaullah Ansari <asanaullah129@gmail.com>
2024-07-21 00:16:38 +0530

    project set up with vite+shadcn+tailwind completed

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+  return (
+    <>
+      <h1>Ai based resume builder</h1>
+      <Button>Let's build it</Button>
+    </>
+  )
+}
+
+export default App
